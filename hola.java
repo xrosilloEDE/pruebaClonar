de50fde1d82a1b2f1091cdcbd19c788ea2d1ac1b
xrosilloEDE <77695291+xrosilloEDE@users.noreply.github.com>
2021-02-02 19:28:46 +0100

    Create hola.java

---
+++ hola.java
@@ -0,0 +1,1 @@
+packageprueba1;publicclassPrincipal {publicstaticvoidmain(String[] args) {System.out.println("Hola mundo java-git");System.out.println("Hola mundo2 java-git");System.out.println("Hola mundo3 java-git");System.out.println("Pruebas Git. Se añade Cambio 1");}}
